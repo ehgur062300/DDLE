3ff34b60dd69cf97a1b77ff92cff400026256e9f
ehgur062300 <ehgur062300@gmail.com>
2024-03-11 14:48:32 +0900

    초기 설정

---
+++ src/main/java/com/example/ddle/DdleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ddle/DdleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
