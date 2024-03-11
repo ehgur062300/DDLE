673c65cf659da868cdf027ba86bba3c6e75e6bbd
ehgur062300 <ehgur062300@gmail.com>
2024-03-11 22:01:14 +0900

    예외 처리 Response

---
+++ src/main/java/com/example/ddle/web/dto/exceptionDto/BasicResponse.java
@@ -0,0 +1,10 @@
+package com.example.ddle.web.dto.exceptionDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public abstract class BasicResponse<T> {
+
+}
